16bb688144aa9a097e6ced372c5419d97cb9eaa5
rc420head <dennislebhardt@yahoo.com>
2015-08-03 23:57:08 -0500

    BUILD_FINGERPRINT cannot contain spaces

--- candy5.mk
+++ candy5.mk
@@ -11,6 +11,5 @@ PRODUCT_NAME := candy5_himawhl
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himawhlspr" \
     PRODUCT_NAME="himawhlspr_na_gen_unlock" \
-    
-BUILD_FINGERPRINT="htc/HTCOneM9spr/htc_himawhl:5.0.2/LRX22G/511781.15:user/release-keys" \
+    BUILD_FINGERPRINT="htc/HTCOneM9spr/htc_himawhl:5.0.2/LRX22G/511781.15:user/release-keys" \
     PRIVATE_BUILD_DESC="1.33.605.15 CL511781 release-keys"
